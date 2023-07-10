be24f56b92a0c85564b578d1a00f05bf11a37fa3
Hemant Prajapati <71485445+hp-coder100@users.noreply.github.com>
2023-07-10 15:52:34 +0530

    Update Navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,13 +10,13 @@ function Navigation() {
     <>
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">A@Z Assignment 1</Navbar.Brand>
+        <Navbar.Brand href="/A2z-assingment/">A@Z Assignment 1</Navbar.Brand>
         <Nav className="ms-auto">
-          <Nav.Link as={RouteLink} to='/' >Task-1</Nav.Link>
+          <Nav.Link as={RouteLink} to='/A2z-assingment/' >Task-1</Nav.Link>
           
-          <Nav.Link as={RouteLink} to='/task2' >Task-2</Nav.Link>
+          <Nav.Link as={RouteLink} to='/A2z-assingment/task2' >Task-2</Nav.Link>
           
-          <Nav.Link as={RouteLink} to='/task3' >Task-3</Nav.Link>
+          <Nav.Link as={RouteLink} to='/A2z-assingment/task3' >Task-3</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
